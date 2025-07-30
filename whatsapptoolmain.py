ba63545fe578f7f46dca4b8a6b2bb22e900607d7
mradhil <ekadhil.com@gmail.com>
2025-07-30 13:38:17 +0530

    Initial commit

--- whatsapptoolmain.py
+++ whatsapptoolmain.py
@@ -3,12 +3,7 @@ import os
 
 
 # ---------- CONFIG ---------- #MESSAGE = "Hello, this is a test message from Python automation!"
-IMAGE_PATH = "path/to/image.jpg"  # Use absolute path
-numbers = ["21423", "7406073514", "6235305505", "9744080036"]
-MESSAGE = "Hello, this is a test message from Python automation!"
-wts = WhatsAppDriver()
-# ---------- MAIN ---------- #
-successful_numbers = []     
-list_wrong_numbers = []
-for number in numbers:
-        wts.send_message_to_contact(number, MESSAGE)
+
+
+
+a=0
